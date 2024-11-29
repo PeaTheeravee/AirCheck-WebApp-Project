5843d5221122d87f28da22838a6197aff70718a1
PeaTheeravee <theeravee238@gmail.com>
2024-11-29 13:57:15 +0700

    edit1 detects

--- backend/notebook/routers/__init__.py
+++ backend/notebook/routers/__init__.py
@@ -1,10 +1,8 @@
 from . import memo
 from . import users
 from . import authentication
-from . import detects
 
 def init_router(app):
     app.include_router(users.router)
     app.include_router(authentication.router)
     app.include_router(memo.router)
-    app.include_router(detects.router)
