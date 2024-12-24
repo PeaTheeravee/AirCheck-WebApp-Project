fdb317e0965c62fa35ee521deadb933ae4899df9
PeaTheeravee <theeravee238@gmail.com>
2024-12-24 11:34:12 +0700

    update home page

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Application!</h1>
+            <p>This is the home page where you can find the latest updates and features.</p>
+        </div>
+    );
+};
+
+export default Home;
